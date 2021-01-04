3dbea556c892b52686c1eadd979765a9214fe149
Thiago <thirauj@br.ibm.com>
2021-01-04 16:58:29 -0300

    Remove old comments

--- helpers/crawler.js
+++ helpers/crawler.js
@@ -24,18 +24,4 @@
             }
         }
     }
-
-	// // Crawl data
-
-	// console.log("Crawling Data...")
-	// HLTV.getMatchesStats({ startDate: '2020-01-01', endDate: '2020-12-31' }).then((res) => {
-	// 	console.log(`\nTotal matches found: ${res.length}`)
-	// 	let date = new Date().getTime();
-	// 	let fileName = `matches_${date}`;
-	// 	fileHelper.saveJson(res, fileName).then(data => {
-	// 		console.log("\nFile saved")
-	// 	}).catch(err => {
-	// 		console.log(err);
-	// 	})
-	// })
 }())
