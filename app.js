4225bcf3568269f42a59f8462b811feb7fe56d33
Thiago <thirauj@br.ibm.com>
2021-01-04 16:57:55 -0300

    Add commentsd

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 (function () {
 	"use strict";
 
+	// Import Helpers
 	let hltvCrawler = require("./helpers/crawler")();
 	let fileHelper = require("./helpers/file")()
 
 	console.log("Crawling Data...")
 
+	// Crawl HTLV Matches
 	hltvCrawler.getMatchData('2020-12-15', '2020-12-31').then(matchData => {
 		console.log(`Total Matches Found: ${matchData.count}`)
 		console.log("Saving the file...")
 
+		// Save the file for future use
         let fileName = `matches_${new Date().getTime()}`;
 		fileHelper.saveJson(matchData, fileName).then(data => {
 			console.log("All Done!")
